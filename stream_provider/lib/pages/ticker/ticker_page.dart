2ea3bb168f759752269f528afecd64180b02aa46
Shiran Kumarasingha <shirankumarasingha97@gmail.com>
2024-04-23 09:56:18 +0530

    finish stream_notifier
    // id we create provider manually it create keepALive ---
    // but we create using code genarater its not Keep Alive

---
+++ stream_provider/lib/pages/ticker/ticker_page.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:stream_provider/pages/ticker/ticker_provider.dart';
+
+class TickerPage extends ConsumerWidget {
+  const TickerPage({super.key});
+
+  String zeroPaddedTwoDigits(double ticks) {
+    return ticks.floor().toString().padLeft(2, '0');
+  }
+
+  String formatTimer(int ticks) {
+    final minutes = zeroPaddedTwoDigits((ticks / 60) % 60);
+    final seconds = zeroPaddedTwoDigits(ticks % 60);
+
+    return '$minutes:$seconds';
+  }
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    print('build');
+    final tickerValue = ref.watch(tickerProvider);
+    print(tickerValue);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Ticker"),
+      ),
+      body: Center(
+        child: tickerValue.when(
+          data: (ticks) => Text(
+            formatTimer(ticks),
+            style: Theme.of(context).textTheme.headlineLarge,
+            textAlign: TextAlign.center,
+          ),
+          error: (e, stackTrace) => Text(
+            e.toString(),
+            style: Theme.of(context).textTheme.headlineSmall,
+            textAlign: TextAlign.center,
+          ),
+          loading: () => CircularProgressIndicator(),
+        ),
+      ),
+    );
+  }
+}
